adc2acf72baf64233fa978357f4706d1bf56cd31
Talut Salako <salakotalut@gmail.com>
2024-06-18 10:04:42 +0100

    cleanup

--- src/middleware/postMiddleware.js
+++ src/middleware/postMiddleware.js
@@ -1,5 +1,3 @@
-import formSchema from "../validation/formSchema.js";
-
 function checkForm(schema) {
   return (req, res, next) => {
     const result = schema.validate(req.body, { abortEarly: false });
